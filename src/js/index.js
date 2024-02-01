e6e1909400e6571ac5cdeca60138d00b956c7946
IhorSlakva <143197433+IhorSlakva@users.noreply.github.com>
2024-02-01 22:22:55 +0200

    homework-11-update

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const input = document.querySelector('.form-input');
+const button = document.querySelector('.form-btn');
